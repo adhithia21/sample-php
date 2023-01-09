f8c60b3a196e3c8744405f87088f3ec51853b96c
adhithia21 <116649263+adhithia21@users.noreply.github.com>
2023-01-09 21:35:30 +0700

    Update welcome_message.php

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -19,6 +19,7 @@
 		<h1>Sample PHP Web</h1>
 		<br />
 		<p><?php echo gethostname(); ?></p>
+		<p>Belajar ansible playbook with role</p>
 		<br />
 	</div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
